278642ff0ca8c1fbf79add50588b751f3e2c8dbc
mervekiratli55 <mervekiratli.mk@gmail.com>
2025-07-11 12:45:23 +0300

    ASP.NET Core Camp: End of day 13

--- obj/Debug/net8.0/TraversalApiProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TraversalApiProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TraversalApiProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+539f9aa7ca0b47f3c54026146050e5831ffaeb6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TraversalApiProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TraversalApiProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
