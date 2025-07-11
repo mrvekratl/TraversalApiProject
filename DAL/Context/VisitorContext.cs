539f9aa7ca0b47f3c54026146050e5831ffaeb6f
mervekiratli55 <mervekiratli.mk@gmail.com>
2025-07-11 12:44:20 +0300

    Initial commit

---
+++ DAL/Context/VisitorContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TraversalApiProject.DAL.Entities;
+
+namespace TraversalApiProject.DAL.Context
+{
+    public class VisitorContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=TraversalApiDb;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+        }
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
